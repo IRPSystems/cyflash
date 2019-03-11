113c6c73de95c0ec6cc47aa6e4bcf3deb2919ad1
Dan Yerushalmi <dan@irpsystems.com>
2019-03-11 14:02:15 +0200

    sending another msg for app specific - blitz

--- cyflash_run.py
+++ cyflash_run.py
@@ -1,3 +1,26 @@
 from cyflash import bootload
 
+import can
+
+print("SENDING RESET MSG")
+bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)
+msg = can.Message(arbitration_id=0x180000DD,data=[0, 0, 0, 0, 0, 0, 0, 0],extended_id=True)
+try:
+	bus.send(msg)
+	print("Message sent on {}".format(bus.channel_info))
+except can.CanError:
+	print("Message NOT sent")
+
+bus.shutdown();
+
+# Call cyflash
 bootload.main()
+
+
+# bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)
+msg = can.Message(arbitration_id=0x180000DD,data=[0, 0, 0, 0, 0, 0, 0, 0],extended_id=True)
+try:
+	bus.send(msg)
+	print("Message sent on {}".format(bus.channel_info))
+except can.CanError:
+	print("Message NOT sent")
