6e1faeaa86bbec698f24a1283ffc184ee9aee2b1
Dan Yerushalmi <dan@irpsystems.com>
2020-11-09 16:42:06 +0200

    updating script to flash oriole BMS

--- cyflash_run.py
+++ cyflash_run.py
@@ -1,63 +1,40 @@
 from cyflash import bootload
 
-import can
+import serial
 import time
 import sys
 
 class Flasher:
 
-	def __init__(self,baud):
+	def __init__(self, baud, com):
 		print("Calling constructor")
-		self.bus = "default"
-		self.baud = int(baud)
+		self.ser = serial.Serial()
+		self.ser.port = com
+		self.ser.baudrate = int(baud)
 		
-	def send_reset(self):
-		msg = can.Message(arbitration_id=0x180000DD,data=[0x8D, 0xC4, 0x55, 0x1B, 0xF0, 0x83, 0x67, 0xEA],extended_id=True)
-		try:
-			self.bus.send(msg)
-			print("Send Reset.. !")
-			#print("Message sent on {}".format(self.bus.channel_info))
-		except can.CanError:
-			print("Send Silence.. error")
+		self.ser.open()
 
-	def send_wake(self):
-		msg = can.Message(arbitration_id=0x180000EE,data=[0, 0, 0, 0, 0, 0, 0, 0],extended_id=True)
-		try:
-			self.bus.send(msg)
-			print("Send wake..")
-			#print("Message sent on {}".format(self.bus.channel_info))
-		except can.CanError:
-			print("Send wake.. error")
+	def send_bootload_enter(self):
+		self.ser.write(b'\x60\x00\x00\x00\x59\xD2')
+		print("Entering bootloader..")
 	
-	def take_bus(self):
-		self.bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=self.baud)
-		
-	def release_bus(self):
-		self.bus.shutdown()
-		
+	def close_connection(self):
+		self.ser.close()
+		print("Finished bootloading")
+	
 	def upload(self):
-		self.take_bus()
-		
-		for i in range(15):
-			self.send_wake()
+		for i in range(5):
+			self.send_bootload_enter()
 			time.sleep(1)
-
-
-		self.send_reset()
-		time.sleep(1)
-		self.send_reset()
-		time.sleep(1)
 		
-		self.release_bus()
+		self.close_connection()
 		bootload.main()
-		self.release_bus()
-	
+		
 
 def main():
-
-
 	filename = sys.argv[1]
 	baud = sys.argv[2]
+	com = sys.argv[3]
 	
 	print("╔╗╔┌─┐─┐ ┬┌─┐┬ ┬┌─┐  ╦╦═╗╔═╗   ╔═╗┬  ┌─┐┌─┐┬ ┬┌─┐┬─┐")  
 	print("║║║├┤ ┌┴┬┘└─┐│ │└─┐  ║╠╦╝╠═╝   ╠╣ │  ├─┤└─┐├─┤├┤ ├┬┘")  
@@ -69,13 +46,13 @@ def main():
 	print("baudrate: " + baud + " bit/sec \r\n")
 	
 	
-	args = 'cyflash_run.py '+ filename + ' --canbus=pcan --canbus_channel=PCAN_USBBUS1 --canbus_id=0x0ab --canbus_baudrate=' + baud
+	args = 'cyflash_run.py '+ filename + ' --serial ' + com + ' --serial_baudrate=' + baud
 	args = args.split()
 	sys.argv = args
 	
-	f = Flasher(baud)
+	f = Flasher(baud, com)
 	f.upload()
 
 
-# Cal main entry point
+# Call main entry point
 main()
